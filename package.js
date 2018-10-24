963d45a05f4703d60b719e5c0fd08cb1d802d798
Nicolas Paton <nicolas.paton@gmail.com>
2018-10-24 22:51:54 +0800

    feat: updated to v0.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "empirica:core",
-  version: "0.0.4",
+  version: "0.0.5",
   summary: "Core Meteor package for the experiment Empirica platform.",
   git: "https://github.com/empiricaly/meteor-empirica-core.git",
   documentation: "README.md"
