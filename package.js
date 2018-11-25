f46a5c31c376a90b141a841a2da3d2bf972c888a
Nicolas Paton <nicolas.paton@gmail.com>
2018-11-25 16:46:46 +0800

    chore(releasing): update isuue and pr templates and package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "empirica:core",
-  version: "0.0.10",
+  version: "0.1.0",
   summary: "Core Meteor package for the experiment Empirica platform.",
   git: "https://github.com/empiricaly/meteor-empirica-core.git",
   documentation: "README.md"
