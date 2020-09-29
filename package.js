1b8e69598986ab2d81f51417621910da9f47f6b0
Hubertus Putu <hubertus.putu1@gmail.com>
2020-09-29 17:13:15 +0800

    chore(release): 1.8.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "empirica:core",
-  version: "1.7.0",
+  version: "1.8.0",
   summary: "Core Meteor package for the experiment Empirica platform.",
   git: "https://github.com/empiricaly/meteor-empirica-core.git",
   documentation: "README.md"
