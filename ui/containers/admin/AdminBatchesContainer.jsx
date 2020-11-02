a30b9f3aedc94f66d61fd117bdc11e969a24bc40
Hubertus Putu Widya Primanta Nugraha <30768964+hubertusputu1@users.noreply.github.com>
2020-11-02 15:18:06 +0800

    fix: change createdAt into runningAt on admin UI #164 ; sort batches in admin UI (#168)
    * fix: change createdAt into runningAt on admin ui #164
    
    * fix: sort batches on admin ui #164
    
    * fix: change name running at into started at #164

--- ui/containers/admin/AdminBatchesContainer.jsx
+++ ui/containers/admin/AdminBatchesContainer.jsx
@@ -20,6 +20,42 @@ export default withTracker(props => {
     {}
   ).ready();
 
+  const batches = Batches.find({
+    archivedAt: { $exists: Boolean(archived) }
+  }).fetch();
+  const sortedBatches = [];
+
+  if (batches.length > 0) {
+    const runningBatches = [];
+    const initBatches = [];
+
+    batches.forEach(b => {
+      switch (b.status) {
+        case "running":
+        case "finished":
+          runningBatches.push(b);
+          break;
+        case "init":
+          initBatches.push(b);
+          break;
+
+        default:
+          sortedBatches.push(b);
+          break;
+      }
+    });
+
+    if (runningBatches.length > 0) {
+      sortedBatches.push(
+        ...runningBatches.sort((a, b) => a.runningAt - b.runningAt)
+      );
+    }
+
+    if (initBatches.length > 0) {
+      sortedBatches.push(...initBatches);
+    }
+  }
+
   return {
     loading:
       batchesLoading ||
@@ -27,9 +63,7 @@ export default withTracker(props => {
       factorsLoading ||
       lobbyConfigsLoading ||
       factorTypesLoading,
-    batches: Batches.find({
-      archivedAt: { $exists: Boolean(archived) }
-    }).fetch(),
+    batches: sortedBatches,
     treatments: Treatments.find().fetch(),
     factors: Factors.find().fetch(),
     factorTypes: FactorTypes.find().fetch(),
