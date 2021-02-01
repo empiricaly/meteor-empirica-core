4d434ef72e8c2d5ba251189fce431eac40b3344f
Hubertus Putu <hubertus.putu1@gmail.com>
2021-02-01 11:42:24 +0800

    fix: remove game helpers #211

--- api/games/augment.js
+++ api/games/augment.js
@@ -1,5 +1,7 @@
 import { Stages } from "../stages/stages";
 import { augmentPlayerStageRound } from "../player-stages/augment";
+import { Rounds } from "../rounds/rounds";
+import { Players } from "../players/players";
 
 export const augmentGameObject = ({
   game,
@@ -27,10 +29,10 @@ export const augmentGameObject = ({
     players: {
       get() {
         if (!gamePlayers) {
-          gamePlayers = game.getPlayers();
+          gamePlayers = Players.find({ _id: { $in: game.playerIds } }).fetch();
 
           if (firstRoundId) {
-            round = game.getRounds().find(r => r._id === firstRoundId);
+            round = Rounds.find({ firstRoundId }).fetch();
             stage = round.stages.find(s => s._id === currentStageId);
           }
 
@@ -47,7 +49,7 @@ export const augmentGameObject = ({
     rounds: {
       get() {
         if (!gameRounds) {
-          gameRounds = game.getRounds();
+          gameRounds = Rounds.find({ gameId: game._id }).fetch();
           gameRounds.forEach(round => {
             let stages = null;
             Object.defineProperty(round, "stages", {
@@ -68,7 +70,7 @@ export const augmentGameObject = ({
     stages: {
       get() {
         if (!gameStages) {
-          gameStages = game.getStages();
+          gameStages = Stages.find({ gameId: game._id }).fetch();
         }
 
         return gameStages;
