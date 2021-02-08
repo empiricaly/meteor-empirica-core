cd8fdb4377520c391899aa42e23834e0a03e19cf
Hubertus Putu Widya Primanta Nugraha <30768964+hubertusputu1@users.noreply.github.com>
2021-02-08 09:55:41 +0800

    chore: release.1.15.0 (#220)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "empirica:core",
-  version: "1.14.0",
+  version: "1.15.0",
   summary: "Core Meteor package for the experiment Empirica platform.",
   git: "https://github.com/empiricaly/meteor-empirica-core.git",
   documentation: "README.md"
