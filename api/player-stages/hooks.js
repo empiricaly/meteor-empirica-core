71bc5dd503ca90a64ec2582bf6ceea217c449f39
Hubertus Putu Widya Primanta Nugraha <30768964+hubertusputu1@users.noreply.github.com>
2021-11-25 10:08:24 +0800

    Fixes/check for early exit player on submitted stage #261 (#262)
    * chore(release): 1.16.2
    
    * chore: fix changelog
    
    * fix: check for exited players when player submit #261
    
    * fix: check for the available playerIDs on the doneCount

--- api/player-stages/hooks.js
+++ api/player-stages/hooks.js
@@ -1,6 +1,7 @@
 // See if everyone is done with this stage
 import { PlayerStages } from "./player-stages";
 import { endOfStage } from "../stages/finish.js";
+import { Players } from "../players/players";
 
 PlayerStages.after.update(
   function(userId, playerStage, fieldNames, modifier, options) {
@@ -9,13 +10,19 @@ PlayerStages.after.update(
     }
     const { stageId } = playerStage;
 
-    const totalCount = PlayerStages.find({ stageId }).count();
+    const playerIDs = PlayerStages.find({ stageId }).map(p => p.playerId);
+    const availPlayerIDs = Players.find({
+      _id: { $in: playerIDs },
+      exitAt: { $exists: false }
+    }).map(p => p._id);
+
     const doneCount = PlayerStages.find({
       stageId,
+      playerId: { $in: availPlayerIDs },
       submittedAt: { $exists: true }
     }).count();
 
-    if (totalCount === doneCount) {
+    if (doneCount === availPlayerIDs.length) {
       endOfStage(stageId);
     }
   },
